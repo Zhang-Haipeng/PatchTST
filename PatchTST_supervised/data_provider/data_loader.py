aaef25f39720035889feb2953268c4698df8fc5a
Roc <roc_zhang@hotmail.com>
2024-07-15 08:31:59 -0400

    run weathre.sh and create learn.ipynb

--- PatchTST_supervised/data_provider/data_loader.py
+++ PatchTST_supervised/data_provider/data_loader.py
@@ -236,8 +236,8 @@ class Dataset_Custom(Dataset):
         num_vali = len(df_raw) - num_train - num_test
         border1s = [0, num_train - self.seq_len, len(df_raw) - num_test - self.seq_len]
         border2s = [num_train, num_train + num_vali, len(df_raw)]
-        border1 = border1s[self.set_type]
-        border2 = border2s[self.set_type]
+        border1 = border1s[self.set_type] # 0 index - begining of num_train
+        border2 = border2s[self.set_type] # end of num_train
 
         if self.features == 'M' or self.features == 'MS':
             cols_data = df_raw.columns[1:]
@@ -266,20 +266,20 @@ class Dataset_Custom(Dataset):
             data_stamp = time_features(pd.to_datetime(df_stamp['date'].values), freq=self.freq)
             data_stamp = data_stamp.transpose(1, 0)
 
-        self.data_x = data[border1:border2]
-        self.data_y = data[border1:border2]
+        self.data_x = data[border1:border2] # num_train
+        self.data_y = data[border1:border2] # num_train
         self.data_stamp = data_stamp
 
     def __getitem__(self, index):
         s_begin = index
         s_end = s_begin + self.seq_len
-        r_begin = s_end - self.label_len
-        r_end = r_begin + self.label_len + self.pred_len
+        r_begin = s_end - self.label_len # begin idx of labels
+        r_end = r_begin + self.label_len + self.pred_len # end idx of labels + pred_len
 
-        seq_x = self.data_x[s_begin:s_end]
-        seq_y = self.data_y[r_begin:r_end]
-        seq_x_mark = self.data_stamp[s_begin:s_end]
-        seq_y_mark = self.data_stamp[r_begin:r_end]
+        seq_x = self.data_x[s_begin:s_end] # batch_x: x including labels
+        seq_y = self.data_y[r_begin:r_end] # batch_y: labels + pred_len
+        seq_x_mark = self.data_stamp[s_begin:s_end] # batch_x_mark: timestamps in a certain format
+        seq_y_mark = self.data_stamp[r_begin:r_end] # batch_y_mark: timestamps in a certain format
 
         return seq_x, seq_y, seq_x_mark, seq_y_mark
 
